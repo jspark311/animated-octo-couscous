974988ca56b16984c1cca7bc3983fa8f67bf3bfd
Ed Gilmore <edgilmore@live.com>
2015-12-12 15:05:15 -0700

    built create form and controller functions

--- myApp/www/js/main.controller.js
+++ myApp/www/js/main.controller.js
@@ -1,15 +1,30 @@
 /**
  * Created by vageeshb on 12/12/15.
  */
-
+'use strict';
 angular.module('SimpleRESTIonic.controllers', [])
 
   .controller('HomeCtrl', function ($rootScope) {
   })
-  .controller('CreateCtrl', function ($rootScope) {
+  .controller('CreateCtrl', function (SessionService, $rootScope, $location) {
+    var vm = this;
+
+    vm.create = function (object) {
+      console.log('create');
+      SessionService.create(object)
+        .then(function () {
+          listSessions();
+        });
+    }
+    function listSessions(){
+      $location.path('/main/join');
+    }
+    vm.cancelCreate = function() {
+      $location.path('/main/home');
+    };
   })
   .controller('JoinCtrl', function ($rootScope, sessions) {
     var vm = this;
-
+    console.log(sessions);
     vm.sessions = sessions.data.data;
   });
